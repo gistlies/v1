22e20c858bb1b3861fab3031c9530881d8aa2612
gistlies <127077501+gistlies@users.noreply.github.com>
2023-03-24 18:31:15 +0700

    Update Splix.lua

--- Libraries/Splix.lua
+++ Libraries/Splix.lua
@@ -358,7 +358,7 @@ do
     function Library:New(info)
 		local info = info or {}
         local name = info.name or info.Name or info.title or info.Title or "UI Title"
-        local size = info.size or info.Size or Vector2.new(504,604)
+        local size = info.size or info.Size or Vector2.new(550,600)
         local accent = info.accent or info.Accent or info.color or info.Color or Theme.accent
         --
         Theme.accent = accent
